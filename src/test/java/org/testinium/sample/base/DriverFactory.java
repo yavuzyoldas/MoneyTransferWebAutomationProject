d3c252686ce499ce73a96f530b8e20399e3ed77e
yavuzahmet <yavuzahmet.yoldas@testinium.com>
2025-02-03 00:09:31 +0300

    unnecessary code lines are removed.

--- src/test/java/org/testinium/sample/base/DriverFactory.java
+++ src/test/java/org/testinium/sample/base/DriverFactory.java
@@ -15,30 +15,6 @@ public class DriverFactory {
 
     private static WebDriver driver;
 
-    // Get WebDriver instance based on browser type
-    /*public static WebDriver getDriver(String browser) {
-        if (driver == null) {
-            System.out.println("driver == null");
-            switch (browser.toLowerCase()) {
-                case "chrome":
-
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    System.out.println("chromeee");
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                // Add more browsers here if needed (e.g., Firefox, Edge, etc.)
-                default:
-                    throw new IllegalArgumentException("Browser " + browser + " not supported.");
-            }
-        }
-        System.out.println("chromeee2");
-        return driver;
-    }*/
-
     public static WebDriver getDriver(String browser) {
         if (driver == null) {
             System.out.println("driver == null");
